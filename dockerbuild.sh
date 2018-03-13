b069666694e311ec794eb27b435b1e8b4600c6a0
Andrea Paolini <ap@nuxi.it>
2018-03-13 12:23:50 +0100

    Add script for building WAR using docker

---
+++ dockerbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## Builds war via a Docker container
+
+docker run -it --rm --name envinfo-docker-builder \
+  -v $(pwd):/usr/src/envinfo -w /usr/src/envinfo \
+  --user="$(id -u):$(id -g)" \
+  jruby:9-alpine sh -c 'bundle install && warble && echo DONE'
+
